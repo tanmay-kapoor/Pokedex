8242e1a11cca50d47111d00cd74173931abb05ec
ptp28 <parthparikh28@gmail.com>
2023-02-17 14:25:43 -0500

    Added model file

---
+++ app/src/main/java/edu/northeastern/pokedex/models/Pokemon.java
@@ -0,0 +1,43 @@
+package edu.northeastern.pokedex.models;
+
+import android.media.Image;
+
+import java.net.URL;
+import java.util.List;
+
+public class Pokemon {
+
+    public String getName() {
+        return name;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public Integer getMoveCount() {
+        return moveCount;
+    }
+
+    public URL getSound() {
+        return sound;
+    }
+
+    private final String name;
+    private final Image image;
+    private final List<String> types;
+    private final Integer moveCount;
+    private final URL sound;
+
+    public Pokemon(String name, Image image, List<String> types, Integer moveCount, URL sound) {
+        this.name = name;
+        this.image = image;
+        this.types = types;
+        this.moveCount = moveCount;
+        this.sound = sound;
+    }
+}
