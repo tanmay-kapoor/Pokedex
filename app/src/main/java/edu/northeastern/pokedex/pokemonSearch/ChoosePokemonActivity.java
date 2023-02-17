11ffa21edd5addeaf3acb64c8f9666574955d4c2
ptp28 <parthparikh28@gmail.com>
2023-02-17 17:45:37 -0500

    Added pagination API calls. Bug fixes.

--- app/src/main/java/edu/northeastern/pokedex/pokemonSearch/ChoosePokemonActivity.java
+++ app/src/main/java/edu/northeastern/pokedex/pokemonSearch/ChoosePokemonActivity.java
@@ -3,10 +3,9 @@ package edu.northeastern.pokedex.pokemonSearch;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
 
@@ -33,26 +32,63 @@ import edu.northeastern.pokedex.utils.NetworkUtil;
 
 public class ChoosePokemonActivity extends AppCompatActivity {
     private List<Pokemon> pokemonList = new ArrayList<>();
-
     private SearchView searchView;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager recyclerLayoutManger;
     private RecyclerAdapter recyclerAdapter;
+    private Button previousButton;
+    private Button nextButton;
+    private String currPokeListLink;
+    private String prevPokeListLink;
+    private String nextPokeListLink;
+    private ProgressBar progressBar;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        currPokeListLink = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
+        prevPokeListLink = null;
+        nextPokeListLink = null;
         setContentView(R.layout.choose_pokemon);
         searchView = findViewById(R.id.pokemonSV);
+        previousButton = findViewById(R.id.prevBtn);
+        nextButton = findViewById(R.id.nextBtn);
+        progressBar = findViewById(R.id.progressBar);
         init(savedInstanceState);
     }
 
+    public void getNextPage(View view) {
+        if(nextPokeListLink == null) {
+            return;
+        }
+        currPokeListLink = nextPokeListLink;
+        recycleRecyclerView();
+    }
+
+    public void getPreviousPage(View view) {
+        if(prevPokeListLink == null) {
+            return;
+        }
+        currPokeListLink = prevPokeListLink;
+        recycleRecyclerView();
+    }
+
     private void init(Bundle savedInstanceState) {
-        // get initial item data
-        Thread getPokemonList = new Thread(new PokemonList());
+        recycleRecyclerView();
+    }
+
+    private void recycleRecyclerView() {
+        //Delete all items from the recyclerview
+        if(pokemonList.size() > 0) {
+            pokemonList.removeIf(pokemon -> true == true);
+            recyclerView.getAdapter().notifyDataSetChanged();
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        //Get new data
+        Thread getPokemonList = new Thread(new PokemonList(currPokeListLink));
         getPokemonList.start();
-        ProgressBar pb = findViewById(R.id.progressBar);
-        pb.setVisibility(View.INVISIBLE);
+        //Create recycler with new data
         createRecyclerView();
     }
 
@@ -88,14 +124,25 @@ public class ChoosePokemonActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(recyclerLayoutManger);
     }
 
+
     private class PokemonList implements Runnable {
-        String pokeListLink = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
+        String pokeListLink;
+
+        PokemonList(String link) {
+            this.pokeListLink = link;
+        }
 
         @Override
         public void run() {
             try {
                 String pokeListResult = NetworkUtil.httpResponse(new URL(pokeListLink));
                 JSONObject pokeListJSON = new JSONObject(pokeListResult);
+                if(pokeListJSON.has("next")) {
+                    nextPokeListLink = String.valueOf(pokeListJSON.get("next"));
+                }
+                if(pokeListJSON.has("previous")) {
+                    prevPokeListLink = String.valueOf(pokeListJSON.get("previous"));
+                }
                 JSONArray pokemonOverview = pokeListJSON.getJSONArray("results");
                 for (int i = 0; i < pokemonOverview.length(); i++) {
                     String pokemonDataResult = NetworkUtil.httpResponse(new URL(String.valueOf(pokemonOverview.getJSONObject(i).get("url"))));
@@ -115,13 +162,19 @@ public class ChoosePokemonActivity extends AppCompatActivity {
                             Integer.valueOf(String.valueOf(pokemonDataJSON.get("id"))),
                             myBitmap
                     ));
-                    recyclerView.getAdapter().notifyItemInserted(pokemonList.size()-1);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    recyclerView.getAdapter().notifyDataSetChanged();
+                }
+            });
         }
     }
 }
