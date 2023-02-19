d15e0ed5a990179d6bf2115dc009821ede6634c1
tanmaykapoor <tanmay.skater@gmail.com>
2023-02-19 14:46:16 -0500

    populate pokemon details activity with api

--- app/src/main/java/edu/northeastern/pokedex/PokemonDetailsActivity.java
+++ app/src/main/java/edu/northeastern/pokedex/PokemonDetailsActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,15 +18,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import edu.northeastern.pokedex.models.Pokemon;
 import edu.northeastern.pokedex.utils.NetworkUtil;
 public class PokemonDetailsActivity extends AppCompatActivity {
 
+    private final Handler handler = new Handler();
+    private TextView pokemonNameText;
+    private TextView moveCountValue;
+    private TextView pokemonTypeList;
+    private ImageView pokemonImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_details);
+
+        pokemonNameText = findViewById(R.id.PokemonName);
+        moveCountValue = findViewById(R.id.MoveCountValue);
+        pokemonTypeList = findViewById(R.id.TypeValues);
+        pokemonImageView = findViewById(R.id.ImageView);
 
         Bundle extras = getIntent().getExtras();
         int id = extras.getInt("pokeID");
@@ -43,8 +60,44 @@ public class PokemonDetailsActivity extends AppCompatActivity {
             try {
                 URL url = new URL(link);
                 String res = NetworkUtil.httpResponse(url);
-                Log.i("data", res);
-            } catch (IOException e) {
+                JSONObject data = new JSONObject(res);
+
+                // get move count
+                int moveCount = data.getJSONArray("moves").length();
+
+                // get list of pokemon types
+                List<String> pokemonType = new ArrayList<>();
+                JSONArray types = data.getJSONArray("types");
+                for(int i = 0; i<types.length(); i++) {
+                    String name = types.getJSONObject(i).getJSONObject("type").getString("name");
+                    String ch = name.charAt(0)+"";
+                    pokemonType.add(name.replace(ch, ch.toUpperCase(Locale.ROOT)));
+                }
+
+                // get pokemon name
+                String name = data.getString("name");
+                String ch = name.charAt(0)+"";
+                final String pokemonName = name.replace(ch, ch.toUpperCase(Locale.ROOT));
+
+                // get image bitmap
+                String imageLink = data.getJSONObject("sprites").getString("front_default");
+                URL urlConnection = new URL(imageLink);
+                HttpURLConnection connection = (HttpURLConnection) urlConnection
+                        .openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap imageBitmap = BitmapFactory.decodeStream(input);
+
+                // update on UI thread
+                handler.post(() -> {
+                    pokemonNameText.setText(pokemonName);
+                    moveCountValue.setText(Integer.toString(moveCount));
+                    pokemonTypeList.setText(String.join(", ", pokemonType));
+                    pokemonImageView.setImageBitmap(imageBitmap);
+                });
+
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
         }
