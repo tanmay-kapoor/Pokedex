d6a5864a958dd1dbe3e126d71c4a26e7f94b9897
supra <rikc.2000@gmail.com>
2023-03-03 20:53:31 -0500

    msg sender layout trial

--- app/src/main/java/edu/northeastern/pokedex/messaging/RecyclerAdapter.java
+++ app/src/main/java/edu/northeastern/pokedex/messaging/RecyclerAdapter.java
@@ -1,11 +1,16 @@
 package edu.northeastern.pokedex.messaging;
 
+import static androidx.preference.PreferenceManager.getDefaultSharedPreferences;
+
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.content.res.AppCompatResources;
@@ -21,10 +26,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerHolder> {
     private final List<Message> messageList;
     private ItemClickListener listener;
     private final Context context;
+    private SharedPreferences prefs;
 
     public RecyclerAdapter(List<Message> messageList, Context context) {
         this.messageList = messageList;
         this.context = context;
+        this.prefs = getDefaultSharedPreferences(context);
     }
 
     public void setOnItemClickListener(ItemClickListener listener) {
@@ -44,6 +51,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerHolder> {
         Message message = messageList.get(position);
         Drawable stickerDrawable = AppCompatResources.getDrawable(context, message.getSticker());
         holder.sticker.setImageDrawable(stickerDrawable);
+        Log.i("USERNAME and SENDER", prefs.getString("username", "not logged in") + message.getSender());
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)holder.sticker.getLayoutParams();
+        if(prefs.getString("username", "not logged in") == message.getSender()) {
+            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        }
+        holder.sticker.setLayoutParams(params);
     }
 
     @Override
