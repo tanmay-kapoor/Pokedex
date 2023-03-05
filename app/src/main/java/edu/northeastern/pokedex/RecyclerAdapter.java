6194d2af6b9582a7cb6a216231a32bce8a2158c7
tanmaykapoor <tanmay.skater@gmail.com>
2023-03-05 17:02:14 -0500

    display receiver name instead of email

--- app/src/main/java/edu/northeastern/pokedex/RecyclerAdapter.java
+++ app/src/main/java/edu/northeastern/pokedex/RecyclerAdapter.java
@@ -15,7 +15,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.Iterator;
 import java.util.List;
 
 import edu.northeastern.pokedex.models.Message;
@@ -59,7 +65,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             Drawable image = AppCompatResources.getDrawable(context, message.getSticker());
             Log.i("RECEIVED", String.valueOf(message.getSticker()));
             receiveHolder.sticker.setImageDrawable(image);
-            receiveHolder.receiverName.setText(message.getSender());
+            setSenderName(receiveHolder, message);
             receiveHolder.receiverDate.setText(message.getDate());
             receiveHolder.receiverTime.setText(message.getTime());
         } else {
@@ -71,6 +77,25 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             senderHolder.senderDate.setText(message.getDate());
             senderHolder.senderTime.setText(message.getTime());
         }
+    }
+
+    private void setSenderName(ReceiverViewHolder holder, Message message) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference ref = mDatabase.child("users").child(message.getUid());
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Iterator<DataSnapshot> children = snapshot.getChildren().iterator();
+                children.next();
+                String name = children.next().getValue().toString();
+                holder.receiverName.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
